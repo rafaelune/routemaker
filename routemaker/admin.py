bb961ca55d5f3413c79ba3b4fba956f1531ba40e
Rafael Une <unesjc@gmail.com>
2012-07-25 21:25:42 -0300

    Ajuste lista e filtro admin, mediaboxes novos

--- routemaker/admin.py
+++ routemaker/admin.py
@@ -1,7 +1,16 @@
+# -*- coding: latin-1 -*-
+
 from django.contrib import admin
 from django.contrib.auth.models import User
 from models import UserProfile
 
 admin.site.unregister(User)
-admin.site.register(UserProfile)
+
+class UserProfileAdmin(admin.ModelAdmin):
+	fields = ('user', 'database')
+	search_fields = ['user', 'database']
+	list_display = ('user', 'database')
+	list_filter = ('user', 'database')
+
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(User)
